d4f7340d5158106aebc3cc391d37c9dd07c8c0f1
Kim Jiwon <stayby16@kakao.com>
2023-08-24 18:18:53 +0900

    리액트 마스터 클래스

---
+++ react-masterclass/src/App.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="teal" />
+    </div>
+  );
+}
+export default App;
