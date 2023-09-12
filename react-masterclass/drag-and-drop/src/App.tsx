b37097338713a2d61f6ed81371ad7e50fca7ddc7
Kim Jiwon <stayby16@kakao.com>
2023-09-12 23:23:50 +0900

    TRELLO CLONE

---
+++ react-masterclass/drag-and-drop/src/App.tsx
@@ -0,0 +1,81 @@
+import {
+  DragDropContext,
+  Draggable,
+  DropResult,
+  Droppable,
+} from "react-beautiful-dnd";
+import { useRecoilState } from "recoil";
+import styled from "styled-components";
+import { toDoState } from "./atoms";
+import DarggableCard from "./Components/DraggableCard";
+import Board from "./Components/Board";
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 680px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Boards = styled.div`
+  display: grid;
+  width: 100%;
+  gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
+`;
+
+function App() {
+  const [toDos, setToDos] = useRecoilState(toDoState);
+  const onDragEnd = (info: DropResult) => {
+    const { destination, draggableId, source } = info;
+    if (!destination) return;
+    if (destination?.droppableId === source.droppableId) {
+      // same board movement
+      setToDos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        const taskObj = boardCopy[source.index];
+        // 1) Delete item on source.index (source.index에서 아이템을 삭제)
+        boardCopy.splice(source.index, 1);
+        // 2) destination.index로 item을 다시 돌려두기
+        boardCopy.splice(destination?.index, 0, taskObj);
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
+    if (destination?.droppableId !== source.droppableId) {
+      // cross board movement
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const taskObj = sourceBoard[source.index];
+        const destinationBoard = [
+          ...allBoards[destination?.droppableId as any],
+        ];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index as any, 0, taskObj);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination?.droppableId as any]: destinationBoard,
+        };
+      });
+    }
+  };
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Wrapper>
+        <Boards>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
+        </Boards>
+      </Wrapper>
+    </DragDropContext>
+  );
+}
+
+export default App;
---
+++ react-masterclass/drag-and-drop/src/Components/DraggableCard.tsx
@@ -0,0 +1,38 @@
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+import React from "react";
+
+const Card = styled.div<{ isDragging: boolean }>`
+  background-color: ${(props) =>
+    props.isDragging ? "#74b9ff" : props.theme.cardColor};
+  box-shadow: ${(props) =>
+    props.isDragging ? "0px 2px 25px rgba(0,0,0,0.5)" : "none"};
+  padding: 10px 10px;
+  border-radius: 5px;
+  margin-bottom: 5px;
+`;
+
+interface IDraggableCardProps {
+  toDoId: number;
+  toDoText: string;
+  index: number;
+}
+
+function DarggableCard({ toDoId, toDoText, index }: IDraggableCardProps) {
+  return (
+    <Draggable draggableId={toDoId + ""} index={index}>
+      {(provided, snapshot) => (
+        <Card
+          isDragging={snapshot.isDragging}
+          ref={provided.innerRef}
+          {...provided.dragHandleProps}
+          {...provided.draggableProps}
+        >
+          {toDoText}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+export default React.memo(DarggableCard);
