452a3b6a71054020b1b23267f77e3379a66def57
Kim Jiwon <stayby16@kakao.com>
2023-10-09 23:16:54 +0900

    🐛 slider part one

--- react-masterclass/nomfilx-clone/nomflix2/src/Routes/Home.tsx
+++ react-masterclass/nomfilx-clone/nomflix2/src/Routes/Home.tsx
@@ -1,11 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "react-query";
-import { getMovies } from "../api";
+import { IGetMoviesResult, getMovies } from "../api";
+import styled from "styled-components";
+import { makeImagePath } from "../utils";
+import { motion, AnimatePresence } from "framer-motion";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    // 선형 그라데이션 효과(시작, 끝)
+    url(${(props) => props.bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+const OverView = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
+
+const Slider = styled.div`
+  position: relative;
+  top: -100px;
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 10px;
+  grid-template-columns: repeat(6, 1fr);
+  width: 100%;
+  position: absolute;
+`;
+
+const Box = styled(motion.div)`
+  background-color: white;
+  height: 200px;
+  color: red;
+  font-size: 64px;
+`;
+
+const rowVariants = {
+  hidden: {
+    x: window.innerWidth,
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: -window.innerWidth - 10,
+  },
+};
 
 function Home() {
-  const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMovies);
-  console.log(data, isLoading);
-  return <div style={{ height: "200vh" }}>Home</div>;
+  const { data, isLoading } = useQuery<IGetMoviesResult>( // useQuery의 결과가 IGetMoviesResult 타입을 가진다.를 의미함.
+    ["movies", "nowPlaying"],
+    getMovies
+  );
+  const [index, setIndex] = useState(0);
+  const increaseIndex = () => setIndex((prev) => prev + 1);
+  return (
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}>
+            {/* 여기서 || "" 는 어떠한 이유에서 data가 존재하지 않아 backdrop_path가 제공되지 않으면 그냥 "" 를 보내라는 의미이다. */}
+            <Title>{data?.results[0].title}</Title>
+            <OverView>{data?.results[0].overview}</OverView>
+          </Banner>
+          <Slider>
+            <AnimatePresence>
+              <Row
+                onClick={increaseIndex}
+                key={index}
+                variants={rowVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ type: "tween", duration: 1 }}
+              >
+                {[1, 2, 3, 4, 5, 6].map((i) => (
+                  <Box key={i}>{i}</Box>
+                ))}
+              </Row>
+            </AnimatePresence>
+          </Slider>
+        </>
+      )}
+    </Wrapper>
+  );
 }
 
 export default Home;
